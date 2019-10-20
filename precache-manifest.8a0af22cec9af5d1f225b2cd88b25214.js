8e01a8403fde01afe2293012449fb0ef99bd54c2
coding-nerdo <brittany.marvin15@yahoo.com>
2019-10-20 13:08:26 -0400

    Updates

---
+++ precache-manifest.8a0af22cec9af5d1f225b2cd88b25214.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55e2146f38062c48acfb9c6aa91cac3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f9e0db9f4d90b6ea3aa",
+    "url": "/static/css/main.4cbbdb1d.chunk.css"
+  },
+  {
+    "revision": "ca17ac7ef88a8c6e1a45",
+    "url": "/static/js/2.a0ef7e58.chunk.js"
+  },
+  {
+    "revision": "7f9e0db9f4d90b6ea3aa",
+    "url": "/static/js/main.4698a266.chunk.js"
+  },
+  {
+    "revision": "ce0fccffc8a0a48d47e0",
+    "url": "/static/js/runtime-main.55a1947a.js"
+  }
+]);
